2682e0639451fb19e2deb68f021e6dce8795baf2
SebaInf <104040673+SebaInf@users.noreply.github.com>
2022-11-11 16:11:52 -0300

    Update __init__.py

--- mod/SGULS/__init__.py
+++ mod/SGULS/__init__.py
@@ -10,72 +10,88 @@ def GS(command):
     time.sleep(0.1)
     
 
-def configGS_CH1(signal="pulse", frec=24000 ,vhl= 0.5 ,vll= -0.5 ,width="NULL"  ,impedance=50, duty="NULL"):
+def configGS_CH1(signal="pulse",polarity="inverted", frec=24000 ,vhl= 0.5 ,vll= -0.5 ,width="NULL"  ,impedance=50, duty="NULL"):
 	GS_OFF_CH1()
 	
+	vamp=vhl-vll
+	
+	GS("OUTP:POL %s" % polarity)
 	GS("func "+ str(signal))
 	GS("VOLT:UNIT VPP")
-	GS("OUTP:LOAD %.1f" % impedance)
+	GS("OUTP:LOAD %.3f" % impedance)
 	GS("FREQ %d" % frec)
-	GS("VOLT:HIGH %.1f" % vhl)	
-	GS("VOLTage:LOW %.1f" % vll)
+	GS("VOLT %.3f" % vamp)
+	GS("VOLT:HIGH %.3f" % vhl)	
+	GS("VOLTage:LOW %.3f" % vll)
 	if width !="NULL":
 		wi=width/1000000000
-		GS("PULS:WIDT %.8f" % wi)	
+		GS("PULS:WIDT %.9f" % wi)	
 	if duty  !="NULL":
-		GS("PULSe:DCYCle %.1f" % duty )
+		GS("PULSe:DCYCle %.3f" % duty )
 		
 		 
 	GS("SYSTem:LOCal")
 	
-def configGS_CH2(signal="pulse", frec=24000 ,vhl= 0.5 ,vll= -0.5 ,width="NULL"  ,impedance=50, duty="NULL"):
+def configGS_CH2(signal="pulse",polarity="norm", frec=24000 ,vhl= 0.5 ,vll= -0.5 ,width="NULL"  ,impedance=50, duty="NULL"):
 	GS_OFF_CH2()
 	
+	vamp=vhl-vll
+	
+	GS("OUTP:POL:CH2 %s" % polarity)
 	GS("func:CH2 "+ str(signal) )
 	GS("VOLT:UNIT:CH2 VPP")
-	GS("OUTP:LOAD:CH2 %.1f" % impedance)
+	GS("OUTP:LOAD:CH2 %.3f" % impedance)
 	GS("FREQ:CH2 %d" % frec)
-	GS("VOLT:HIGH:CH2 %.1f" % vhl)	
-	GS("VOLTage:LOW:CH2 %.1f" % vll)
+	GS("VOLT %.3f" % vamp)
+	GS("VOLT:HIGH:CH2 %.3f" % vhl)	
+	GS("VOLTage:LOW:CH2 %.3f" % vll)
 	if width !="NULL":
 		wi=width/1000000000
-		GS("PULS:WIDT %.8f" % wi)	
+		GS("PULS:WIDT %.9f" % wi)	
 	if duty  !="NULL":
-		GS("PULSe:DCYCle %.1f" % duty )
+		GS("PULSe:DCYCle %.3f" % duty )
 		
 		 
 	GS("SYSTem:LOCal")
 	
 	
-def configGS_ALL(signal="pulse", frec=24000 ,vhl= 0.5 ,vll= -0.5 ,width="NULL"  ,impedance=50, duty="NULL"):
+def configGS_ALL(signal="pulse",polarity="norm", frec=24000 ,vhl= 0.5 ,vll= -0.5 ,width="NULL"  ,impedance=50, duty="NULL"):
 	GS_OFF_ALL()
 		
+	vamp=vhl-vll	
+	
+	GS("OUTP:POL %s" % polarity)
+	GS("OUTP:POL:CH2 %s" % polarity)
+	
 	GS("func "+ str(signal) )
 	GS("func:CH2 "+ str(signal) )
 	
 	GS("VOLT:UNIT VPP")
 	GS("VOLT:UNIT:CH2 VPP")
 	
-	GS("OUTP:LOAD %.1f" % impedance)
-	GS("OUTP:LOAD:CH2 %.1f" % impedance)
+	GS("OUTP:LOAD %.3f" % impedance)
+	GS("OUTP:LOAD:CH2 %.3f" % impedance)
 	
 	GS("FREQ %d" % frec)
 	GS("FREQ:CH2 %d" % frec)
 	
-	GS("VOLT:HIGH %.1f" % vhl)	
-	GS("VOLT:HIGH:CH2 %.1f" % vhl)	
+	GS("VOLT %.3f" % vamp)
+	GS("VOLT:CH2 %.3f" % vamp)
 	
-	GS("VOLTage:LOW %.1f" % vll)
-	GS("VOLTage:LOW:CH2 %.1f" % vll)
+	GS("VOLT:HIGH %.3f" % vhl)	
+	GS("VOLT:HIGH:CH2 %.3f" % vhl)	
 	
-	if duty !="NULL":
+	GS("VOLTage:LOW %.3f" % vll)
+	GS("VOLTage:LOW:CH2 %.3f" % vll)
+	
+	if width !="NULL":
 		wi=width/1000000000
-		GS("PULS:WIDT %.8f" % wi)	
-		GS("PULS:WIDT:CH2 %.8f" % wi)	
+		GS("PULS:WIDT %.9f" % wi)	
+		GS("PULS:WIDT:CH2 %.9f" % wi)	
 		
 	if duty !="NULL":
-		GS("PULSe:DCYCle %.1f" % duty )
-		GS("PULSe:DCYCle:CH2 %.1f" % duty )
+		GS("PULSe:DCYCle %.3f" % duty )
+		GS("PULSe:DCYCle:CH2 %.3f" % duty )
 		 
 	GS("SYSTem:LOCal")
 	
