1b4d16b980b9669084d2a78b079ab32465024122
SebaInf <104040673+SebaInf@users.noreply.github.com>
2023-03-08 16:32:01 -0300

    Add files via upload

---
+++ red_pitaya/turnoff.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+var=$(/home/src/lago -a)
+#echo $var
+
+prefix="#Trigger Level Ch1 = 500
+#Trigger Level Ch2 = 9000
+#High Voltage 1    = "
+foo=${var#"$prefix"}
+suffix=" mV
+#High Voltage 2    = 7.7 mV
+#Trigger Scaler 1  = 1
+#Trigger Scaler 2  = 1
+#No GPS device is present or enabled
+#Working mode is MASTER
+
+Status from registers complete!"
+foo=${foo%"$suffix"}
+
+foo=${foo%\.*}
+echo $foo
+
+hv1_i=$foo
+hv1_f=300
+hv1_a=$((hv1_i))
+rest=3
+
+/home/src/lago -s hv1 ${hv1_a}
+sleep $rest
+
+while [ $hv1_a -ge $((hv1_f+1)) ] 
+do 
+	hv1_a=$((hv1_a-200))
+	echo $hv1_a	
+	/home/src/lago -s hv1 ${hv1_a}
+	sleep $rest
+done
+hv1_a=0
+echo $hv1_a
+/home/src/lago -s hv1 ${hv1_a}
+sleep $rest
+
+echo "apagando"
+
+poweroff
