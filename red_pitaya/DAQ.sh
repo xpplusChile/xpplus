050fd574016800ccd7d5219c8ab9d6d60628c923
SebaInf <104040673+SebaInf@users.noreply.github.com>
2023-03-08 16:33:24 -0300

    Update DAQ.sh

--- red_pitaya/DAQ.sh
+++ red_pitaya/DAQ.sh
@@ -2,11 +2,18 @@
 
 process_id=`/bin/ps -fu $USER| grep "bin/sh /home/src/startup.sh" | grep -v "grep" | awk '{print $2}'`	
 if [ -z "$process_id" ]
+
+hour=$(date +%H)
+trigger=$((hour*100))
+#/home/src/lago -s t1 ${trigger}
+echo $trigger
 then
-	#echo "\$var is empty"
-	timeout 1h /home/src/lago -f /home/src/datos/	
+	echo "\$var is empty"
+	#timeout 1h /home/src/lago -f /home/src/datos/	
 else
-    #echo "\$var is NOT empty"
+    echo "\$var is NOT empty"
 fi
+
+
 
 
