10ab16d57f9dc6a8e200198d44be666e2d52ad69
xpplusChile <129527670+xpplusChile@users.noreply.github.com>
2023-04-25 15:23:34 -0400

    Update startup.sh

--- red_pitaya/startup.sh
+++ red_pitaya/startup.sh
@@ -24,20 +24,22 @@ printf "\n_________________________________________________________" >> /home/sr
 #####################################################################################################
 ## startup
 
+hv1_i=300
+hv1_f=1300
+t1=1000
+hv1_a=$((hv1_i))
+
 sleep 10
 touch MASTER
 sleep 1
-cat /home/lago_v1_3.bit > /dev/xdevcfg
+cat /home/src/lago_v1_2.bit > /dev/xdevcfg
 sleep 1
-/home/src/lago -s t1 170
+/home/src/lago -s t1 ${t1}
 sleep 1
 /home/src/lago -s t2 9000
 sleep 1
 /home/src/lago -s hv1 0
 sleep 1
-hv1_i=300
-hv1_f=1300
-hv1_a=$((hv1_i))
 
 /home/src/lago -s hv1 ${hv1_i}
 sleep 5
@@ -46,22 +48,14 @@ do
 	hv1_a=$((hv1_a+200))
 	echo $hv1_a	
 	/home/src/lago -s hv1 ${hv1_a}
-	sleep 50
+	sleep 10
 done
-
+/home/src/lago -s hv1 ${hv1_f}
+sleep 10
+	
 /home/src/lago -s hv2 0
 sleep 1 
-process_id=`/bin/ps -fu $USER| grep "bin/sh /home/src/DAQ.sh" | grep -v "grep" | awk '{print $2}'`	
-if [ -z "$process_id" ]
-then
-	echo "\$var is empty"
-    minuto=$(date +%M)  
-	segundo=$(date +%S) 
-	tiempo_t=$((59 * 60 -minuto*60 - segundo + 58 ))
-	echo "$tiempo_t"
-	timeout ${tiempo_t}s /home/src/lago -f /home/src/datos/ 
-	
-else
-      echo "\$var is NOT empty"
-fi
+#timeout 60s /home/src/lago -f /home/src/datos/hv1_${hv1_f}_t1_${t1}
+/home/src/lago -f /home/src/datos/hv1_${hv1_f}_t1_${t1}
+
 
